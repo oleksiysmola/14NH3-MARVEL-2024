6c861a3d994a31b075d8394c0d2065476bc3871a
alex smola <asmola@flo.theory.phys.ucl.ac.uk>
2023-10-31 14:43:13 +0000

    extracted 21CeCaCo transitions

--- 21CeCaCo/convertToMarvel-14NH3.py
+++ 21CeCaCo/convertToMarvel-14NH3.py
@@ -5,27 +5,49 @@ pandarallel.initialize(progress_bar=True)
 df = pd.read_csv("21CeCaCoWithoutHeader.txt", delim_whitespace=True)
 
 
+# df["Assigned"] = df.notna().all(axis=1)
+# df = df[df["Assigned"] == True]
+# df = df.drop("Assigned", axis=1)
+df = df[df["C37"].notna()]
+# print(len(df))
+# print(df.head(15).to_string(index=False))
+df["Columns missing"] = df["C39"].isna()
+
+def shiftColumns(row):
+    columns = row.index
+    print(columns)
+    if row["Columns missing"]:
+        i = -1
+        running = True
+        while running:
+            if columns[i] == "C12":
+                break
+            row[columns[i]] = row[columns[i - 2]]
+            i -= 1
+    return row
+
+df = df.parallel_apply(lambda x:shiftColumns(x), result_type="expand", axis=1)
+
+
+
 df["Uncertainty"] = df["C4"]
 columns = ["C3", "C4", "Uncertainty"] + [f"C{i}" for i in range(14, 20)] + ["C22", "C23", "C20", "C25", "C26"]
 columns += [f"C{i}" for i in range(27, 33)] + ["C35", "C36", "C33", "C38", "C39"]
 df = df[columns]
 
-# Obtain assigned lines
-df["Assigned"] = df.notna().all(axis=1)
-df = df[df["Assigned"] == True]
-df = df.drop("Assigned", axis=1)
-print(len(df))
+print(df.head(20).to_string(index=False))
+columns = df.columns.tolist()
+i = 3
+while i < len(columns):
+    df[columns[i]] = df[columns[i]].astype(int)
+    i += 1
+
 
 
 # 14NH3
 # df["C.14"] = df[df["C.14"].astype(int) == 1]
 # df = df.drop("C.14", axis=1)
 # print(len(df))
-columns = df.columns.tolist()
-i = 3
-while i < len(columns):
-    df[columns[i]] = df[columns[i]].astype(int)
-    i += 1
     
 
 # print(df.head(15).to_string(index=False))
@@ -52,6 +74,7 @@ df["C38"] = df["C38"].map(symmetryMapping)
 
 df = df.reset_index()
 df["Source"] = pd.DataFrame({"Source": [f"21CeCaCo.{i + 1}" for i in range(len(df))]})
+df = df.drop("index", axis=1)
 # df = df.drop("index", axis=1)
 print(df.head(15).to_string(index=False))
 df = df.to_string(index=False)
