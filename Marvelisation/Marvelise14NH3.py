3ea6860e686290357664db03a5c5bf69df5c647f
alex smola <asmola@vincent.theory.phys.ucl.ac.uk>
2023-11-21 16:55:23 +0000

    Unformatted Marvelisation complete

--- Marvelisation/Marvelise14NH3.py
+++ Marvelisation/Marvelise14NH3.py
@@ -1,11 +1,23 @@
 import pandas as pd
-from math import isnan
 from pandarallel import pandarallel
 pandarallel.initialize(progress_bar=True)
+
+pd.set_option('display.float_format', '{:.6f}'.format)
 
 marvelColumns = ["nu1", "nu2", "nu3", "nu4", "L3", "L4", "J", "K", "inv", "Gamma", "Nb", "Em", "Uncertainty", "Transitions"]
 
 marvelEnergies = pd.read_csv("14NH3-NewEnergies.txt", delim_whitespace=True, names=marvelColumns, dtype=str)
+
+symmetryMap = {
+    "A1'": "1",
+    "A2'": "2",
+    "E'": "3",
+    "A1\"": "4",
+    "A2\"": "5",
+    "E\"": "6"
+}
+
+marvelEnergies["Gamma"] = marvelEnergies["Gamma"].map(symmetryMap)
 
 statesFileColumns = ["i", "E", "g", "J", "weight", "p", "Gamma", "Nb", "n1", "n2", "n3", "n4", "l3", "l4", "inversion", "J'", "K'", "pRot", "v1", "v2", "v3", "v4", "v5", "v6", "GammaVib", "Calc"]
 states = pd.read_csv("../14N-1H3__CoYuTe.states", delim_whitespace=True, names=statesFileColumns, dtype=str)
@@ -17,28 +29,64 @@ def generateTagColumn(dataFrame):
 marvelEnergies = generateTagColumn(marvelEnergies)
 states = generateTagColumn(states)
 
-states = pd.merge(states, marvelEnergies[["Tag", "Em", "Uncertainty"]], how="left")
+columnsToConvertToInteger = ["J","n1", "n2", "n3", "n4"]
+for column in columnsToConvertToInteger:
+    states[column] = states[column].astype(int)
+
+states = pd.merge(states, marvelEnergies[["Tag", "Em", "Uncertainty"]], on="Tag", how="left")
+# print(states[states["Tag"] == ""].head(20).to_string(index=False))
+
+columnsToConvertToFloat = ["Em", "E", "Calc", "weight", "Uncertainty"]
+for column in columnsToConvertToFloat:
+    states[column] = states[column].astype(float)
+    
+pd.set_option('display.float_format', '{:.6f}'.format)
 
 def marvelise(row):
     # Parameters that get used for the calculated uncertainty
     deltaB = 0.002
     deltaOmega = 0.3
-    if isnan(row["Em"]):
-        row["E"] = row["Calc"]
-        row["Marvel"] = "Ca"
-        row["weight"] = deltaB*int(row["J"])*(int(row["J"]) + 1) + deltaOmega*(int(row["n1"]) + int(row["n2"]) + int(row["n3"]) + int(row["n4"]))
-    else:
-        row["E"] = row["Em"]
+    calculatedEnergy = row["Calc"]
+    if row["Em"] >= 0:
         row["Marvel"] = "Ma"
-        row["weight"] = row["Uncertainty"]
+        marvelUncertainty = row["Uncertainty"] 
+        row["weight"] = f"{marvelUncertainty:.{6}f}"
+        marvelEnergy = row["Em"] 
+        row["E"] = f"{marvelEnergy:.{6}f}" 
+    else:
+        row["Marvel"] = "Ca"
+        estimatedUncertainty = deltaB*row["J"]*(row["J"] + 1) + deltaOmega*(row["n1"] + row["n2"] + row["n3"] + row["n4"])
+        row["weight"] = f"{estimatedUncertainty:.{6}f}"
+        row["E"] = f"{calculatedEnergy:.{6}f}"
+    row["Calc"] = f"{calculatedEnergy:.{6}f}"
     return row
 
+print("\n")
+print("Beginning Marvelisation...")
 states = states.parallel_apply(lambda x:marvelise(x), result_type="expand", axis=1)
+print("\n")
+print("Marvelisation complete!")
 statesFileColumns = ["i", "E", "g", "J", "weight", "p", "Gamma", "Nb", "n1", "n2", "n3", "n4", "l3", "l4", "inversion", "J'", "K'", "pRot", "v1", "v2", "v3", "v4", "v5", "v6", "GammaVib", "Marvel", "Calc"]
 states = states[statesFileColumns]
+
+
+def formatCell(value):
+    return f'{value: >12}'
+
+columnsToFormat = ['i', 'E', 'Calc', 'weight']
+statesColumnsToFormat = states[columnsToFormat]
+states = states.drop(columnsToFormat, axis=1)
+statesColumnsToFormat = statesColumnsToFormat.applymap(formatCell)
+states = pd.concat([states, statesColumnsToFormat], axis=1, join="inner")
+states = states[statesFileColumns]
+
+pd.set_option('display.float_format', '{:.6f}'.format)
+
 print(states.head(20).to_string(index=False))
 
+print("Printing states file...")
 states = states.to_string(index=False, header=False)
 statesFile = "14N-1H3__CoYuTe-Marvelised-2024.states"
 with open(statesFile, "w+") as FileToWriteTo:
     FileToWriteTo.write(states)
+print("New states file ready!")
