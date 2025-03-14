5f71d360c52c0e01813386b3dbf6ad4c4d8f51f0
Oleksiy <oleksiy.smola@gmail.com>
2025-03-14 16:51:15 +0000

    created MARVEL energy level and uncertainty plot

--- CombinationDifferencesTests/CheckForMissingMarvel2020Levels.py
+++ CombinationDifferencesTests/CheckForMissingMarvel2020Levels.py
@@ -1,6 +1,9 @@
 import pandas as pd
 from pandarallel import pandarallel
 pandarallel.initialize(progress_bar=True)
+import matplotlib.pyplot as plt
+import scienceplots
+plt.style.use('science')
 
 marvelColumns = ["nu1", "nu2", "nu3", "nu4", "L3", "L4", "J", "K", "inv", "Gamma", "Nb", "E", "Uncertainty", "Transitions"]
 
@@ -34,7 +37,31 @@ print(len(missing2020Levels))
 print(missing2020Levels.to_string(index=False))
 
 marvelEnergiesNew = marvelEnergiesNew.parallel_apply(lambda x:checkIfEnergyIsNew(x, marvelEnergies2020), result_type="expand", axis=1)
+marvelEnergiesOld = marvelEnergiesNew[marvelEnergiesNew["New"] == False]
 marvelEnergiesNew = marvelEnergiesNew[marvelEnergiesNew["New"]]
+
+fontsize=30
+# plt.plot(marvelEnergiesNew["J"], marvelEnergiesNew["E"], "b.", label="This work")
+# plt.plot(marvelEnergiesOld["J"], marvelEnergiesOld["E"], "r.", label="20FuCoTe")
+# plt.xlim(0, 31)
+# plt.xticks(fontsize=fontsize)
+# plt.yticks(fontsize=fontsize)
+# plt.xlabel("J", fontsize=fontsize)
+# plt.ylabel("E, cm$^{-1}$", fontsize=fontsize)
+# plt.legend(prop={"size": 30}, frameon=True)
+# plt.show()
+
+# marvelEnergiesNewOneTransition = marvelEnergiesNew[marvelEnergiesNew["Transitions"] == 1]
+# plt.plot(marvelEnergiesNew["E"], marvelEnergiesNew["Uncertainty"], "b.")
+# plt.plot(marvelEnergiesNewOneTransition["E"], marvelEnergiesNewOneTransition["Uncertainty"], "r.")
+# plt.xlim(3700, 19000)
+# plt.ylim(0, 0.05)
+# plt.xticks(fontsize=fontsize)
+# plt.yticks(fontsize=fontsize)
+# plt.xlabel("E, cm$^{-1}$", fontsize=fontsize)
+# plt.ylabel("Uncertainty, cm$^{-1}$", fontsize=fontsize)
+# # plt.legend(prop={"size": 30}, frameon=True)
+# plt.show()
 
 symmetryMap = {
     "A1'": "1",
@@ -50,7 +77,7 @@ marvelEnergiesNew = marvelEnergiesNew.parallel_apply(lambda x:assignMarvelTags(x
 
 statesFileColumns = ["i", "Energy", "g", "J", "weight", "p", "Gamma", "Nb", "nu1", "nu2", "nu3", "nu4", "L3", "L4", "inv", "J'", "K'", "pRot", "v1", "v2", "v3", "v4", "v5", "v6", "GammaVib", "Calc"]
 states = pd.read_csv("../14N-1H3__CoYuTe.states", delim_whitespace=True, names=statesFileColumns)
-states = states[states["Energy"] < 8000]
+states = states[states["Energy"] < 19000]
 states = states[states["g"] > 0]
 # states = states[states["J"] <= 8] 
 
@@ -59,6 +86,19 @@ print(states.head(20).to_string(header=False))
 marvelEnergiesNew = marvelEnergiesNew.drop("New", axis=1)
 marvelEnergiesNew = pd.merge(marvelEnergiesNew, states[["Tag", "Energy"]], on="Tag", how="left")
 marvelEnergiesNew["Obs-Calc"] = abs(marvelEnergiesNew["E"] - marvelEnergiesNew["Energy"])
+
+marvelEnergiesNewOneTransition = marvelEnergiesNew[marvelEnergiesNew["Transitions"] == 1]
+plt.plot(marvelEnergiesNew["E"], marvelEnergiesNew["Obs-Calc"], "b.")
+plt.plot(marvelEnergiesNewOneTransition["E"], marvelEnergiesNewOneTransition["Obs-Calc"], "r.")
+# plt.xlim(3700, 19000)
+# plt.ylim(0, 0.05)
+plt.yscale("log")
+plt.xticks(fontsize=fontsize)
+plt.yticks(fontsize=fontsize)
+plt.xlabel("E, cm$^{-1}$", fontsize=fontsize)
+plt.ylabel(r"$|$Obs-Calc$|$, cm$^{-1}$", fontsize=fontsize)
+# plt.legend(prop={"size": 30}, frameon=True)
+plt.show()
 marvelEnergiesNew = marvelEnergiesNew.to_string(index=False, header=False)
 newEnergiesFile = "MARVEL-NewEnergiesComparison-2024.states"
 with open(newEnergiesFile, "w+") as FileToWriteTo:
